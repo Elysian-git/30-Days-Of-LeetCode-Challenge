94c04479fef5ed02ca7b6c74c44c7855a85ed981
Elysian-git <itszimaldarr@gmail.com>
2025-08-03 21:10:20 +0500

    Update 605: Can Place Flowers (Easy).py

--- 605: Can Place Flowers (Easy).py
+++ 605: Can Place Flowers (Easy).py
@@ -6,33 +6,16 @@ class Solution:
         count = 0
         length = len(flowerbed)
 
-        for i in range(length):
-            if n == 0:
-                return True
+        if n == 0:
+            return True
 
-            if (i==0) or flowerbed[i-1]==0:
-                if (i==length-1) or flowerbed[length-1]==0:
+        for i in range(length):
+            if flowerbed[i]==0:
+                left_empty = (i==0) or (flowerbed[i-1]==0)
+                right_empty = (i==length-1) or (flowerbed[i+1]==0)
+                if left_empty and right_empty:
+                    flowerbed[i] = 1
                     count += 1
                     if count >= n:
                         return True
-        return False
-
-# Solution 2
-# ----------------------O(len(flowerbed)) TC ----------------- O(1) SC ---------- 
-
-class Solution:
-    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-        count = 0
-        length = len(flowerbed)
-
-        for i in range(length):
-            if n == 0:
-                return True
-
-            if ((i==0) or flowerbed[i-1]==0) and ((i==length-1) or flowerbed[length-1]==0):
-                count += 1
-                if count >= n:
-                    return True
-        return False
-
-        
+        return count >= n
