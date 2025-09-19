cd4708b07f5fbbda14fce680907a953a4da42108
Elysian-git <itszimaldarr@gmail.com>
2025-09-19 22:26:56 +0500

    Update 11: Longest Common Prefix (easy).py

--- 11: Longest Common Prefix (easy).py
+++ 11: Longest Common Prefix (easy).py
@@ -1,4 +1,4 @@
-# SOLUTION 1
+# SOLUTION 1 ( Horizontal scanning )
 # ------------------ O(n * m) TC ----------- O(1) SC --------
 
 class Solution:
@@ -15,7 +15,23 @@ class Solution:
         return prefix
 
 
-# Solution 2
+# SOLUTION 2 ( Vertical scanning )
+# ------------------ O(n * m) TC ----------- O(1) SC --------
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs:
+            return ""
+        
+        for i in range(len(strs[0])):  # check each char of first word
+            c = strs[0][i]
+            for word in strs[1:]:
+                if i == len(word) or word[i] != c:
+                    return strs[0][:i]
+        return strs[0]
+
+
+# Solution 3
 # ------------------ O(n log (n * m)) TC ----------- O(1) SC --------
 
   class Solution:
@@ -31,3 +47,27 @@ class Solution:
                 return ans
             ans += first[i]          # otherwise, add char to answer
         return ans
+
+# Solution 4: Divide and Conquer
+# ------------------ O(n·m) TC ----------- O(m·log n) SC --------
+
+class Solution:
+    def lcp(self, left: str, right: str) -> str:
+        i = 0
+        while i < len(left) and i < len(right) and left[i] == right[i]:
+            i += 1
+        return left[:i]
+
+    def helper(self, strs, l, r):
+        if l == r:
+            return strs[l]
+        mid = (l + r) // 2
+        left = self.helper(strs, l, mid)
+        right = self.helper(strs, mid+1, r)
+        return self.lcp(left, right)
+
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs:
+            return ""
+        return self.helper(strs, 0, len(strs)-1)
+
